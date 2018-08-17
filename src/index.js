7faf473f8f52e995cbec5620ab73645540e3d622
Ryan <ryanliu2k5@gmail.com>
2018-08-17 11:47:04 +1000

    3rd commit: Add webpack and loaders with 3 stage configs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App from './components/App';
+
+import './styles/style.sass';
 
 render(
   <App />,
