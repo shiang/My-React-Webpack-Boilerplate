de0b6e2ede739b83841eeb2bfef192762475df8b
Ryan <ryanliu2k5@gmail.com>
2018-08-19 17:18:18 +1000

    11th commit: Add customized UglifyJsPlugin config to optimize CSS asset and change config for MiniCssExtractPlugin(filename/chunkfileName) for production.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -16,19 +18,27 @@ module.exports = merge(common, {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: "css-loader"
           },
           {
-            loader: "sass-loader",
+            loader: "sass-loader"
           }
         ]
-      },
+      }
     ]
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
-    }
+      chunks: "all"
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
